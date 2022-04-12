bf262aff4c07d83e86d43e758670dfe26bb3e0b3
FinlayWilliams <57364056+FinlayWilliams@users.noreply.github.com>
2022-04-12 19:29:57 +0100

    Tweaks to all pages

--- models/sis.py
+++ models/sis.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.integrate import odeint
-from sklearn.linear_model import LinearRegression
+
 
 class SIS:
     # Preliminary Constructor to Initialise Variables and Allow for easy class creation
